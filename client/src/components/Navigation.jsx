d2eb2080f6de9b1be13bc400f24331a860b34960
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-04-26 15:47:56 +0100

    Restarting entiree design

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../assets/Logo.png";
-import { MdRssFeed } from "react-icons/md";
+import profilepicture from "../assets/profilepic.jpeg";
+import { MdHome, MdRssFeed } from "react-icons/md";
 import {
   BsPersonPlusFill,
   BsBriefcase,
@@ -11,58 +12,53 @@ import {
 import { IoIosNotificationsOutline } from "react-icons/io";
 const Navigation = () => {
   return (
-    <nav className="">
-      <ul className="flex justify-between gap-5 items-center p-2 h-[80px] w-full shadow-lg">
-        <div className="w-[130px] h-[80px] border-r-2 border-gray-100">
-          {" "}
-          <img src={logo} className="w-[46px] h-[46px] mx-auto mt-[17px] " />
+    <nav className=" sticky top-0 flex p-5 justify-between w-full text-xs h-20 mx-auto my-0 min-h-[100%] ">
+      <div className="flex justify-between gap-5 items-center">
+        <img src={logo} className="h-8 w-8" />
+        <div>
+          <span>
+            <BsSearch />
+          </span>
+          Search
         </div>
-        <li className="w-[90px] text-center">
-          <span className=" text-center">
-            <MdRssFeed className="mx-auto w-[24px] h-[24px] " /> Feed
-          </span>
-        </li>
-        <li className="w-[90px] text-center ">
-          <span className=" ">
-            <BsPersonPlusFill Feed className=" w-[24px] h-[24px] mx-auto " />{" "}
-            Network
-          </span>
-        </li>
-        <li className="w-[90px] text-center">
-          <span className=" text-center">
-            <BsBriefcase className="mx-auto w-[24px] h-[24px] " /> Jobs
-          </span>
-        </li>
-        <li className="w-[90px] text-center">
-          <span className=" text-center">
-            <BsChatLeft className="mx-auto w-[24px] h-[24px] " /> Chat
-          </span>
-        </li>
-        <li className="w-[90px] text-center">
-          <span className=" text-center">
-            <IoIosNotificationsOutline className="mx-auto w-[24px] h-[24px] " />{" "}
-            Notifications
-          </span>
-        </li>
-        <div className="relative w-[367px] border border-x-2 border-gray-100">
-          <BsSearch className="absolute left-2 top-8 mr-10" />
-          <input
-            type="search"
-            placeholder="Search"
-            className="w-[360px] h-[80px] p-5 ml-4 outline-none"
-          />
-        </div>
-        <li className="w-[90px] text-center">
-          <span className=" text-center">
-            <MdRssFeed className="mx-auto w-[24px] h-[24px] " /> Feed
-          </span>
-        </li>
-        <li className="w-[90px] text-center">
-          <span className=" text-center">
-            <BsThreeDots className="mx-auto w-[24px] h-[24px] " /> Other
-          </span>
-        </li>
-      </ul>
+      </div>
+
+      <div>
+        <ul className="flex gap-5 items-center">
+          <li>
+            <span>
+              <MdHome />
+            </span>
+            Home
+          </li>
+          <li>
+            <span>
+              <BsPersonPlusFill />
+            </span>
+            My Network
+          </li>
+          <li>
+            <span>
+              <BsBriefcase />
+            </span>
+            Jobs
+          </li>
+          <li>Messaging</li>
+          <li>Notifications</li>
+          <li>
+            <img
+              src={profilepicture}
+              className="w-5 h-5 rounded-full"
+              alt="A profile picture"
+            />
+            Me
+          </li>
+        </ul>
+      </div>
+      <div className="flex gap-5 items-center">
+        <div>For Business</div>
+        <a href="#">Get Hired Faster. Try Premium Free</a>
+      </div>
     </nav>
   );
 };
