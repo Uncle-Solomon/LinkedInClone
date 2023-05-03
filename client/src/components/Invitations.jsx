bf96142dca1de8c870e64b7a9e8d54fecf58e9d0
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-05-03 17:41:10 +0100

    Starting InvitationsComponent

--- client/src/components/Invitations.jsx
+++ client/src/components/Invitations.jsx
@@ -11,7 +11,7 @@ const Invitations = () => {
           See all <span>5</span>
         </p>
       </div>
-      <div className="flex justify-between px-4 pt-4 pb-2 border-b mb-2">
+      <div className="flex justify-between items-center px-4 pt-4 pb-2 border-b mb-2">
         <div className="flex gap-2">
           <img
             className="w-16 h-16 rounded-full border border-white"
@@ -25,8 +25,74 @@ const Invitations = () => {
           </div>
         </div>
         <div>
-          <button type="button">Ignore</button>
-          <button type="button">Accept</button>
+          <button
+            type="button"
+            className="mx-2 hover:bg-gray-200 py-1 px-3 font-bold"
+          >
+            Ignore
+          </button>
+          <button
+            type="button"
+            className="mx-2 hover:bg-gray-200 rounded-2xl py-1 px-3 border border-blue-600 text-blue-600 font-bold"
+          >
+            Accept
+          </button>
+        </div>
+      </div>
+      <div className="flex justify-between items-center px-4 pt-4 pb-2 border-b mb-2">
+        <div className="flex gap-2">
+          <img
+            className="w-16 h-16 rounded-full border border-white"
+            src={ebuka}
+          />
+          <div className="p-2 text-sm text-gray-400 ">
+            <h2 className="text-black leading-2 font-bold">
+              Chukwuebuka Udeala
+            </h2>
+            <p>Chukwuebuka Udeala wants to connect</p>
+          </div>
+        </div>
+        <div>
+          <button
+            type="button"
+            className="mx-2 hover:bg-gray-200 py-1 px-3 font-bold"
+          >
+            Ignore
+          </button>
+          <button
+            type="button"
+            className="mx-2 hover:bg-gray-200 rounded-2xl py-1 px-3 border border-blue-600 text-blue-600 font-bold"
+          >
+            Accept
+          </button>
+        </div>
+      </div>
+      <div className="flex justify-between items-center px-4 pt-4 pb-2 border-b mb-2">
+        <div className="flex gap-2">
+          <img
+            className="w-16 h-16 rounded-full border border-white"
+            src={ebuka}
+          />
+          <div className="p-2 text-sm text-gray-400 ">
+            <h2 className="text-black leading-2 font-bold">
+              Chukwuebuka Udeala
+            </h2>
+            <p>Chukwuebuka Udeala wants to connect</p>
+          </div>
+        </div>
+        <div>
+          <button
+            type="button"
+            className="mx-2 hover:bg-gray-200 py-1 px-3 font-bold font-bold"
+          >
+            Ignore
+          </button>
+          <button
+            type="button"
+            className="mx-2 hover:bg-gray-200 rounded-2xl py-1 px-3 border border-blue-600 text-blue-600 font-bold font-bold"
+          >
+            Accept
+          </button>
         </div>
       </div>
     </div>
--- client/src/pages/Network.jsx
+++ client/src/pages/Network.jsx
@@ -9,10 +9,10 @@ const Network = () => {
     <div className=" bg-gray-100 min-h-[100vh]">
       <Navigation />
       <div className=" grid grid-cols-12 mt-5 px-5 md:px-10 lg:px-20 ">
-        <div className=" col-span-3">
+        <div className=" col-span-12 md:col-span-4 lg:col-span-3">
           <SideNavNetwork />
         </div>
-        <div className=" col-span-9 px-2">
+        <div className=" col-span-12 md:col-span-8 lg:col-span-9 px-2">
           <Invitations />
           <Alumni />
         </div>
