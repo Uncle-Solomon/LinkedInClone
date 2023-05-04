91d364f1614600bd73bbcf0634cef3c05ea62c3e
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-05-04 10:18:55 +0100

    Working on singula user component

--- client/src/components/Alumni.jsx
+++ client/src/components/Alumni.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import SingularUserCard from "./SingularUserCard";
 
 const Alumni = () => {
   return (
     <div className="bg-white w-full text-sm">
       <div className="flex justify-between px-4 pt-4 pb-2 border-b mb-2">
-        <p>Top emerging creators to follow</p>
+        <p>
+          People you may know from <span>Covenant University</span>
+        </p>
         <p>See all</p>
+      </div>
+      <div className="grid grid-cols-12">
+        <SingularUserCard />
+        <SingularUserCard />
+        <SingularUserCard />
+        <SingularUserCard />
+        <SingularUserCard />
+        <SingularUserCard />
+        <SingularUserCard />
+        <SingularUserCard />
       </div>
     </div>
   );
