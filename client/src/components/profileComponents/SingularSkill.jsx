6af01d7f38fbf114ca94897af65e5372379b4e68
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-05-10 17:39:36 +0100

    Working on profile components

---
+++ client/src/components/profileComponents/SingularSkill.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SingularSkill = () => {
+  return (
+    <div className="text-xs mt-2">
+      <p className="font-semibold my-2">Javascript</p>
+      <p className="font-extralight">
+        Junior Software and Embedded Systems Engineer at Inspired Technologies
+      </p>
+    </div>
+  );
+};
+
+export default SingularSkill;
--- client/src/components/profileComponents/Skills.jsx
+++ client/src/components/profileComponents/Skills.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import { BsPencil } from "react-icons/bs";
+import { BiRightArrowAlt } from "react-icons/bi";
+import SingularSkill from "./SingularSkill";
 
 const Skills = () => {
   return (
-    <div className="rounded-md p-2 w-[95%] mx-auto shadow-md  z-0 bg-white my-2 ">
-      Skills
+    <div className="rounded-md p-6 w-[95%] mx-auto shadow-md  z-0 bg-white my-2 ">
+      <div>
+        <div className="flex justify-between">
+          <h1>Skills</h1>
+          <div className="flex gap-2 cursor-pointer">
+            <button className="mx-2 py-1 px-2 border border-blue-600 text-blue-600 rounded-2xl font-semibold text-sm  hover:bg-blue-100">
+              Demonstrate Skills
+            </button>
+            <AiOutlinePlus />
+            <BsPencil />
+          </div>
+        </div>
+        <SingularSkill />
+        <SingularSkill />
+      </div>
+      <div className="flex items-center w-full  hover:bg-gray-300 gap-2 justify-center text-sm mt-2 p-2 ">
+        Show all <span>15 </span>skills <BiRightArrowAlt />
+      </div>
     </div>
   );
 };
