fa0cefbf19e77465b530c9e77bb77adcbc78d8e2
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-05-17 10:11:35 +0100

    Working on landing page

--- client/src/components/landingPageComponents/OpenToWork.jsx
+++ client/src/components/landingPageComponents/OpenToWork.jsx
@@ -3,19 +3,19 @@ import opentowork from "../../assets/landing.png";
 
 const OpenToWork = () => {
   return (
-    <div className="grid grid-cols-10 space-x-16 w-full py-2 lg:pl-56 lg:pr-12 lg:mr-12 my-8 bg-gray-100">
-      <div className="col-span-10 md:col-span-10 lg:col-span-3 p-1">
+    <div className="grid grid-cols-10 h-[85vh] space-x-16 w-full lg:pl-56 lg:pr-12 lg:mr-12  bg-gray-100">
+      <div className="col-span-10 md:col-span-10 my-auto lg:col-span-4 p-1">
         <h1 className=" text-amber-900 text-4xl font-normal w-[90%] my-10 leading-11">
           Let the right people know you’re open to work
         </h1>
-        <p>
+        <p className="text-black text-3xl font-extralight w-[90%] my-10 leading-11">
           With the Open To Work feature, you can privately tell recruiters or
           publicly share with the LinkedIn community that you are looking for
           new job opportunities.
         </p>
       </div>
-      <div className="col-span-10 md:col-span-10 lg:col-span-4 p-1 space-x-2">
-        <img src={opentowork} />
+      <div className="col-span-10 md:col-span-10  my-auto lg:col-span-4 p-1 space-x-2">
+        <img src={opentowork} className="h-[24rem] w-[24rem]" />
       </div>
     </div>
   );
