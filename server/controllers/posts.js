7cc4469470ab170700cd8db8d9c0ca94a334bacb
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-06-05 17:50:38 +0100

    Making Progress

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,5 @@
 import Post from "../db/models/Posts.js";
+import User from "../db/models/Users.js";
 
 export const createPost = async (req, res) => {
   const post = await Post.create(req.body);
@@ -7,5 +8,6 @@ export const createPost = async (req, res) => {
 
 export const getAllPosts = async (req, res) => {
   const post = await Post.find({});
+  // const user = post.user;
   res.status(200).json({ post });
 };
